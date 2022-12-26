63c302816f59fa53376ff7ba9af645336eb227d9
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-12-26 21:19:18 +0200

    COMMIT

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,47 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( e => {
+player.on('timeupdate',  throttle(e => {
     localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-    );
+}, 1000)
+);
 
 player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
 .catch(function (error) {
-    console.error(error)
+    console.error(error);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
