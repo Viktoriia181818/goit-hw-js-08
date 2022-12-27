b0d08e52c572cffbddf2fcaf3a2f02755e5d06ae
Viktoriia181818 <115534675+Viktoriia181818@users.noreply.github.com>
2022-12-27 10:57:51 +0000

    Deploying to gh-pages from @ Viktoriia181818/goit-hw-js-08@b4c618fff202d78ef9bcd97ba4a40f6410eda2c6 🚀

--- src/js/03-feedback.js
+++
@@ -1,9 +0,0 @@
-import { throttle } from "lodash";
-
-const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name = email]');
-const message = document.querySelector('textarea[name = message]');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-
-
-
